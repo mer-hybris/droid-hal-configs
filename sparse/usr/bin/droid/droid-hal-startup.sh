afecc986f60a2a7b35ff37acb36bcfeaa393e909
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2017-01-30 21:47:19 +0200

    [dhc] Remove call to no-longer-existing android-permission-fixup.sh
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jollamobile.com>

--- sparse/usr/bin/droid/droid-hal-startup.sh
+++ sparse/usr/bin/droid/droid-hal-startup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd /
-sh /usr/libexec/droid/android-permission-fixup.sh &> /dev/null
 touch /dev/.coldboot_done
 export LD_LIBRARY_PATH=/usr/libexec/droid-hybris/system/lib/:/vendor/lib:/system/lib
 
